239420c42299275fcca7380494a81952f5fc672a
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2021-04-03 22:29:26 +0200

    setup of socket io connection

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,39 @@
 import express from "express";
 import http from "http";
-import {configs} from "./config.js";
-import * as exerciseDatabaseManager from "./src/exercises/exerciseDatabaseManager.js";
+import { configs } from "./config.js";
+import { Server } from "socket.io"
+import cors from "cors";
+
 
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
 
 const ip = process.env.SERVER_IP || configs.ip;
 const port = process.env.SERVER_PORT || configs.port;
 
-app.get("/" , (req,res) => {
+app.get("/", (req, res) => {
     res.send("Hello World");
 })
 
 server.listen(port, ip, () => {
     console.log(`Server starts running on ${ip}:${port}`);
+});
+
+io.on("connection", (socket) => {
+    console.log("new socket connection");
+    socket.emit("testevent", { a: 5 });
+    console.log(io.engine.clientsCount);
+
+    socket.on('disconnect', function () {
+        console.log('socket disconnect!');
+    });
 });
 
 

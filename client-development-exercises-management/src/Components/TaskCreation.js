1b247a694dea27c64c4192a162c2ff57b0c47588
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2021-04-03 20:12:43 +0200

    Added error alert when name, score or importance are not set. Day is only selectable when week is selected

--- client-development-exercises-management/src/Components/TaskCreation.js
+++ client-development-exercises-management/src/Components/TaskCreation.js
@@ -16,38 +16,38 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
 const scoresMenuItems = [
-    {value : 1, label : 1},
-    {value : 2, label : 2},
-    {value : 3, label : 3},
-    {value : 4, label : 4},
-    {value : 5, label : 5},
-    {value : 6, label : 6},
-    {value : 7, label : 7},
-    {value : 8, label : 8},
-    {value : 9, label : 9},
-    {value : 10, label : 10},
+    { value: 1, label: 1 },
+    { value: 2, label: 2 },
+    { value: 3, label: 3 },
+    { value: 4, label: 4 },
+    { value: 5, label: 5 },
+    { value: 6, label: 6 },
+    { value: 7, label: 7 },
+    { value: 8, label: 8 },
+    { value: 9, label: 9 },
+    { value: 10, label: 10 },
 ];
 
 const importencesMenuItems = [
-    {value : 1, label : 1},
-    {value : 2, label : 2},
-    {value : 3, label : 3},
+    { value: 1, label: 1 },
+    { value: 2, label: 2 },
+    { value: 3, label: 3 },
 ]
 
 const weeklyRythmsMenuItems = [
-    {value : "weekly" , label : "weekly"},
-    {value : "bi-weekly" , label : "bi-weekly"},
-    {value : "three-week" , label : "three-week"}
+    { value: "weekly", label: "weekly" },
+    { value: "bi-weekly", label: "bi-weekly" },
+    { value: "three-week", label: "three-week" }
 ]
 
 const daysMenuItems = [
-    {value : 0 , label : "Monday"},
-    {value : 1 , label : "Tuesday"},
-    {value : 2 , label : "Wednesday"},
-    {value : 3 , label : "Thursday"},
-    {value : 4 , label : "Friday"},
-    {value : 5 , label : "Saturday"},
-    {value : 6 , label : "Sunday"}
+    { value: 0, label: "Monday" },
+    { value: 1, label: "Tuesday" },
+    { value: 2, label: "Wednesday" },
+    { value: 3, label: "Thursday" },
+    { value: 4, label: "Friday" },
+    { value: 5, label: "Saturday" },
+    { value: 6, label: "Sunday" }
 ]
 
 
@@ -58,7 +58,7 @@ const useStyles = makeStyles({
     formControl: {
         minWidth: 150,
     },
-    informationText : {
+    informationText: {
         marginTop: "40px",
         marginBottom: "20px"
     }
@@ -69,7 +69,11 @@ function ValueMenuSelection(props) {
     const classes = useStyles();
 
     return (
-        <FormControl variant="outlined" className={classes.formControl}>
+        <FormControl 
+            variant="outlined" 
+            className={classes.formControl}
+            disabled={props.disabled || false}
+        >
             <InputLabel>{props.label}</InputLabel>
             <Select
                 value={props.value}
@@ -94,82 +98,91 @@ function ValueMenuSelection(props) {
 export function TaskCreation(props) {
     const classes = useStyles();
 
-    const [open, setOpen] = useState(false);
+    const [openAddTask, setOpenAddTask] = useState(false);
+    const [openErrorAlert, setOpenErrorAlert] = useState(false);
     const [state, setState] = useState({
-        name : "",
-        score : "",
-        importance : "",
-        weeklyRythm : "",
-        day : ""
+        name: "",
+        score: "",
+        importance: "",
+        weeklyRythm: "",
+        day: ""
     });
 
-    const [age, setAge] = useState('');
-
-    const handleClickOpen = () => {
-        setOpen(true);
+    const handleOpenAddTask = () => {
+        setOpenAddTask(true);
     };
 
-    const handleClose = () => {
-        setOpen(false);
+    const handleCloseAddTask = () => {
+        setState({
+            name: "",
+            score: "",
+            importance: "",
+            weeklyRythm: "",
+            day: ""
+        })
+        setOpenAddTask(false);
+    };
+
+    const handleOpenErrorAlert = () => {
+        setOpenErrorAlert(true);
+    };
+
+    const handleCloseErrorAlert = () => {
+        setOpenErrorAlert(false);
     };
 
     const handleChangeName = (e) => {
         setState({
             ...state,
-            name : e.target.value
+            name: e.target.value
         });
     }
 
     const handleChangeScore = (e) => {
         setState({
             ...state,
-            score : e.target.value
+            score: e.target.value
         });
     }
 
     const handleChangeImportance = (e) => {
         setState({
             ...state,
-            importance : e.target.value
+            importance: e.target.value
         })
     }
 
     const handleChangeWeeklyRythm = (e) => {
         setState({
-            ...state, 
-            weeklyRythm : e.target.value
+            ...state,
+            weeklyRythm: e.target.value
         })
     }
 
     const handleChangeDay = (e) => {
         setState({
             ...state,
-            day : e.target.value
+            day: e.target.value
         })
     }
 
     const handleChangeText = (e) => {
         setState({
             ...state,
-            name : e.target.value
+            name: e.target.value
         })
     }
 
     const handleSubmit = () => {
-        console.log(state);
         let isNameSet = state.name != "";
-        let isScoreSet = state.name != "";
-        let isImportenceSet = state.name != "";
-        
-        if(!isNameSet){
+        let isScoreSet = state.score != "";
+        let isImportenceSet = state.importance != "";
+
+        if (!isNameSet || !isScoreSet || !isImportenceSet) {
+            setOpenErrorAlert(true);
             return;
         }
-        if(!isScoreSet){
-            return;
-        }
-        if(!isImportenceSet){
-            return;
-        }
+        console.log(state);
     }
 
     return (
@@ -179,11 +192,11 @@ export function TaskCreation(props) {
                 color="secondary"
                 className={classes.root}
                 startIcon={<DeleteIcon />}
-                onClick={handleClickOpen}
+                onClick={handleOpenAddTask}
             >
                 Add Task
             </Button>
-            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" maxWidth={'sm'} fullWidth={true}>
+            <Dialog open={openAddTask} onClose={handleCloseAddTask} aria-labelledby="form-dialog-title" maxWidth={'sm'} fullWidth={true}>
                 <DialogTitle id="form-dialog-title">Add Task</DialogTitle>
                 <DialogContent>
                     <TextField
@@ -192,57 +205,76 @@ export function TaskCreation(props) {
                         id="taskname"
                         label="Name of the Task"
                         type="name"
-                        onChange = {handleChangeText}
+                        onChange={handleChangeText}
                     />
                     <DialogContentText className={classes.informationText}>
                         Choose an importance value. A high value ensure that this Task rather occurs at the top in the presentation view
                     </DialogContentText>
-                    <ValueMenuSelection 
+                    <ValueMenuSelection
                         value={state.score}
                         label={"Score"}
                         menuItems={scoresMenuItems}
-                        handleChange = {handleChangeScore}
+                        handleChange={handleChangeScore}
                     />
 
                     <DialogContentText className={classes.informationText}>
                         Choose a Score value which is obtained after solving this Task
                     </DialogContentText>
-                    <ValueMenuSelection 
+                    <ValueMenuSelection
                         value={state.importance}
                         label={"Importance"}
                         menuItems={importencesMenuItems}
-                        handleChange = {handleChangeImportance}
+                        handleChange={handleChangeImportance}
                     />
 
-                    <DialogContentText  className={classes.informationText}>
+                    <DialogContentText className={classes.informationText}>
                         Optional : Select a rythm indicating on which weeks this Task should appear (starting from current week)
                     </DialogContentText>
-                    
-                    <ValueMenuSelection 
+
+                    <ValueMenuSelection
                         value={state.weeklyRythm}
                         label={"Weekly Rythm"}
                         menuItems={weeklyRythmsMenuItems}
-                        handleChange = {handleChangeWeeklyRythm}
+                        handleChange={handleChangeWeeklyRythm}
                     />
 
-                    <DialogContentText  className={classes.informationText}>
+                    <DialogContentText className={classes.informationText}>
                         Optional : If the Task is day dependant, you can select the respective day which gets attached to the Task name in the presentation view
                     </DialogContentText>
-                    <ValueMenuSelection 
+                    <ValueMenuSelection
                         value={state.day}
                         label={"Day"}
                         menuItems={daysMenuItems}
-                        handleChange = {handleChangeDay}
+                        handleChange={handleChangeDay}
+                        disabled={state.weeklyRythm === ""}
                     />
 
                 </DialogContent>
 
                 <DialogActions>
-                    <Button onClick={handleClose} color="primary">
+                    <Button onClick={handleCloseAddTask} color="primary">
                         Cancel
                     </Button>
                     <Button onClick={handleSubmit} color="primary">
                         Add Task
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog
+                open={openErrorAlert}
+                onClose={handleCloseErrorAlert}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">Error when trying to create Task</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Be sure that you set a task name, its respective score and an importance value!
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseErrorAlert} color="primary" autoFocus>
+                        Agree
                     </Button>
                 </DialogActions>
             </Dialog>

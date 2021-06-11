ed50dc99c4a074a810dd7d3b9e7334541d923ccc
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2021-06-11 18:12:36 +0200

    added score of 0

--- client-development-exercises-management/src/constants.ts
+++ client-development-exercises-management/src/constants.ts
@@ -4,6 +4,7 @@ interface ValueLabel {
 }
 
 export const MENU_ITEMS_SCORES : ValueLabel[] = [
+    { value: 0, label: 0 },
     { value: 1, label: 1 },
     { value: 2, label: 2 },
     { value: 3, label: 3 },

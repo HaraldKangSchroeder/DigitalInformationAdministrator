e1360cccf8fb82d158fb11f8f779b0c1d4c9af49
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2021-06-11 20:01:35 +0200

    Prepend 0 when hour or minute is a single value

--- client-development-app/src/Components/Clock.tsx
+++ client-development-app/src/Components/Clock.tsx
@@ -10,9 +10,23 @@ export default function Clock(){
         }, 1000);
     },[]);
 
+    let hours = time.getHours() + "";
+    let isHoursSingleValue = hours.length === 1;
+    if(isHoursSingleValue) hours = prependZero(hours);
+
+    let minutes = time.getMinutes() + "";
+    let isMinutesSingleValue = minutes.length === 1;
+    if(isMinutesSingleValue) minutes = prependZero(minutes);
+
     return (
         <div>
-            {time.getHours()}:{time.getMinutes()}
+            {hours}:{minutes}
         </div>
     );
+}
+
+function prependZero(value : string) : string{
+    value = "" + value;
+    if(value.length === 1) return "0" + value;
+    return value;
 }

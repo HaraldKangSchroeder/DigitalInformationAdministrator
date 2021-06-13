891f0bc47039206b8cabd077bb980ed2abe97866
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2021-06-13 10:53:13 +0200

    Fixed Error -> Now, Sunday is the first day of week

--- Server/src/utils.js
+++ Server/src/utils.js
@@ -1,8 +1,10 @@
 exports.getWeekNumberByDate = (d) => {
-    // TODO seems like on saturday a new week begins already
-    var onejan = new Date(d.getFullYear(), 0, 1);
+    var copiedDate = new Date(d.getTime());
+    // add one day => week will then already start at sunday
+    copiedDate.setDate(d.getDate() + 1);
+    var onejan = new Date(copiedDate.getFullYear(), 0, 1);
     var millisecsInDay = 86400000;
-    return Math.ceil((((d - onejan) / millisecsInDay) + onejan.getDay()) / 7);
+    return Math.ceil((((copiedDate.getTime() - onejan.getTime()) / millisecsInDay) + onejan.getDay()) / 7);
 };
 
 exports.getMillisecondsByMinute = (min) => {

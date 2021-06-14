d4c12310e92d739c14c3e4137dbf0695e1e94970
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2021-06-14 17:01:40 +0200

    Refactoring

--- client-development-app/src/Classes/Tasks.ts
+++ client-development-app/src/Classes/Tasks.ts
@@ -12,18 +12,22 @@ export default class Tasks {
         let meanSaturation = 93;
         let numberOfDistinctTasks = getNumberOfDistinctTasks(tasksDataset);
         let hslList = getHslList(numberOfDistinctTasks, lightness, meanSaturation);
-        for(let i = 0, j = -1; i < tasksDataset.length; i++){
-            if(!this.containsTaskByTaskId(tasksDataset[i].taskId)){
+        for (let i = 0, j = -1; i < tasksDataset.length; i++) {
+            if (!this.containsTaskByTaskId(tasksDataset[i].taskId)) {
                 j++;
             }
-            let task = new Task(tasksDataset[i].id, tasksDataset[i].taskId, tasksDataset[i].userId, tasksDataset[i].label, tasksDataset[i].score, tasksDataset[i].dayOfWeek, hslList[j]);
-            this.taskList.push(task);
+            this.addTask(tasksDataset[i].id, tasksDataset[i].taskId, tasksDataset[i].userId, tasksDataset[i].label, tasksDataset[i].score, tasksDataset[i].dayOfWeek, hslList[j]);
         }
     }
 
-    containsTaskByTaskId(taskId : number) : boolean{
-        for(let task of this.taskList){
-            if(task.getTaskId() == taskId){
+    addTask(id: number, taskId: number, userId: number, label: string, score: number, dayOfWeek: number, color: string) {
+        let task = new Task(id, taskId, userId, label, score, dayOfWeek, color);
+        this.taskList.push(task);
+    }
+
+    containsTaskByTaskId(taskId: number): boolean {
+        for (let task of this.taskList) {
+            if (task.getTaskId() == taskId) {
                 return true;
             }
         }
@@ -35,10 +39,10 @@ export default class Tasks {
     }
 }
 
-function getNumberOfDistinctTasks(tasksDataset : any){
-    let taskIds : number[] = [];
-    for(let i = 0; i < tasksDataset.length; i++){
-        if(!taskIds.includes(tasksDataset[i].taskId)){
+function getNumberOfDistinctTasks(tasksDataset: any) {
+    let taskIds: number[] = [];
+    for (let i = 0; i < tasksDataset.length; i++) {
+        if (!taskIds.includes(tasksDataset[i].taskId)) {
             taskIds.push(tasksDataset[i].taskId);
         }
     }

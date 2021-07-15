2132cb795aaad4e890e944f979d9d9b2d09050b3
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2021-07-16 00:30:53 +0200

    tasksaccomplishmentclasses for usersOverview

---
+++ client-development-app/src/Classes/TaskAccomplishment.ts
@@ -0,0 +1,30 @@
+export default class TaskAccomplishment {
+
+    taskId: number;
+    userId: number;
+    calendarWeek: number;
+    year: number;
+
+    constructor(taskId: number, userId: number, calendarWeek: number, year: number) {
+        this.taskId = taskId;
+        this.userId = userId;
+        this.calendarWeek = calendarWeek;
+        this.year = year;
+    }
+
+    getTaskId(): number {
+        return this.taskId;
+    }
+
+    getUserId(): number {
+        return this.userId;
+    }
+
+    getCalendarWeek(): number {
+        return this.calendarWeek;
+    }
+
+    getYear(): number {
+        return this.year;
+    }
+}

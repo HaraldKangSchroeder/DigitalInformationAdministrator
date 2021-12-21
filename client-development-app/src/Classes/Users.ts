62967c70798f0e81fb6020edb92a8631703ce851
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2021-12-21 13:36:24 +0100

    fix user presentation error when weeklyscore is 0

--- client-development-app/src/Classes/Users.ts
+++ client-development-app/src/Classes/Users.ts
@@ -3,46 +3,40 @@ import { getHslList } from "../utils";
 
 export default class Users {
 
-    userList : User[];
+    userList: User[];
 
-    constructor(users? : any){
+    constructor(users?: any) {
         this.userList = [];
-        if(users == null) return;
+        if (users == null) return;
         let lightness = 50;
         let meanSaturation = 85;
         let hslList = getHslList(users.length, lightness, meanSaturation);
-        for(let i = 0; i < users.length; i++){
-            let user;
-            if(users[i].scoreOfWeek){
-                user = new User(users[i].id,users[i].name,users[i].scoreOfWeek,users[i].scoreOfYear,hslList[i]);
-            }
-            else{
-                user = new User(users[i].id,users[i].name,0,0,"#000000");
-            }
+        for (let i = 0; i < users.length; i++) {
+            let user = new User(users[i].id, users[i].name, users[i].scoreOfWeek, users[i].scoreOfYear, hslList[i]);
             this.addUser(user);
         }
     }
 
-    addUser(user : User){
+    addUser(user: User) {
         this.userList.push(user);
     }
 
-    getList() : User[]{
+    getList(): User[] {
         return this.userList;
     }
 
-    getUserById(userId : number) : User {
-        for(let user of this.userList) {
-            if(user.getId() === userId){
+    getUserById(userId: number): User {
+        for (let user of this.userList) {
+            if (user.getId() === userId) {
                 return user;
             }
         }
         return null;
     }
 
-    getUserIds() : number[] {
+    getUserIds(): number[] {
         let ids = [];
-        for(let user of this.userList){
+        for (let user of this.userList) {
             ids.push(user.getId());
         }
         return ids;

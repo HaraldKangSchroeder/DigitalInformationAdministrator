03ee1a2ac9121444092d282a5c62b81833b36802
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2021-12-23 14:37:42 +0100

    clear interval after view change

--- client-development-app/src/Components/Navbar/Clock.tsx
+++ client-development-app/src/Components/Navbar/Clock.tsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react"
 
 
-export default function Clock(){
-    const [time,setTime] = useState(new Date());
+export default function Clock() {
+    const [time, setTime] = useState(new Date());
 
     useEffect(() => {
         const interval = setInterval(() => {
             setTime(new Date());
         }, 1000);
-    },[]);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
 
     return (
         <div>
@@ -17,19 +21,19 @@ export default function Clock(){
     );
 }
 
-export function getTimeRepresentation(time : Date) : string{
+export function getTimeRepresentation(time: Date): string {
     let hours = time.getHours() + "";
     let isHoursSingleValue = hours.length === 1;
-    if(isHoursSingleValue) hours = prependZero(hours);
+    if (isHoursSingleValue) hours = prependZero(hours);
 
     let minutes = time.getMinutes() + "";
     let isMinutesSingleValue = minutes.length === 1;
-    if(isMinutesSingleValue) minutes = prependZero(minutes);
+    if (isMinutesSingleValue) minutes = prependZero(minutes);
     return `${hours}:${minutes}`;
 }
 
-export function prependZero(value : string) : string{
+export function prependZero(value: string): string {
     value = "" + value;
-    if(value.length === 1) return "0" + value;
+    if (value.length === 1) return "0" + value;
     return value;
 }

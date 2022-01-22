571beaacc08e7710ffbe87d50a081dc1671180e2
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2022-01-22 17:38:42 +0100

    moved mobile app to groceryCart project

--- mobile-app/src/GroceryCart/app/src/main/java/com/example/grocerycart/GroceryCartManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.grocerycart;
-
-public class GroceryCartManager {
-
-    public static GroceryCart groceryCart = new GroceryCart();
-    public static GroceryTypes groceryTypes = new GroceryTypes();
-    public static Groceries groceries = new Groceries();
-
-}
